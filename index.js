a7b93c6368b3c5e6b210bfb0d9c7dab28cd08cb9
Javier Cisneros <fjca185@gmail.com>
2021-12-08 21:55:15 -0600

    Inicio de proyecto

--- index.js
+++
@@ -1,9 +0,0 @@
-const math = require('./math.js');
-//Se llama al objeto importado con require
-math("Javier");
-//Se le llama la funcion
-/*console.log('Suma:', math.add(1,0));
-console.log('Resta: ', math.substract(1,0));
-console.log('Multiplicación: ' + math.multiply(1,0));
-console.log('División: ' + math.divide(1,0)); 
-console.log('División: ' + math.divide(1,2)); */
